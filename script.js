f4a9a16d5dbc8b66b34d928fe94606f3dad7e782
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-05-26 23:54:02 +0200

    changes

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 const sentenceTag = document.querySelector(`input[type="text"]`);
-const outputTag = document.querySelector(".output");
+const outputTag = document.querySelector("textarea.output");
+const originalText = outputTag.value; //that is used to save the initial value of the innerHTML in order to reuse it
 
 //when I type in my sentence tag, update the output tag
-
+// if there's no value, put in the original text
 sentenceTag.addEventListener("keyup", (e) => {
-  outputTag.innerHTML = sentenceTag.value;
+  if (sentenceTag.value) {
+    outputTag.value = sentenceTag.value;
+  } else {
+    outputTag.value = originalText; //original text is used here and value because there is no inner HTML but a value, the content inbetween the textarea is a value and not innerHTML
+  }
+});
+
+// when i type in my output tag, update the sentence tag accordingly
+
+outputTag.addEventListener("keyup", function () {
+  sentenceTag.value = this.value;
 });
